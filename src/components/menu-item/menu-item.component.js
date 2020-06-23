58388d0fec1e132b0de650a6a6452b0c19559825
anasiba <anasibarki1997@gmail.com>
2020-06-23 05:35:55 +0100

    Add router functionality to menu items

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+
 import './menu-item.styles.scss'
 
-export default function MenuItem({ title, imageUrl, size }) {
+function MenuItem({ title, imageUrl, size, history, linkUrl, match}) {
     return (
-        <div className={`${size} menu-item`} >
+        <div className={`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)} >
             <div className="background-image"
-                 style={{
+                style={{
                     backgroundImage: `url(${imageUrl})`
-                 }}/>
+                }} />
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
@@ -16,3 +18,5 @@ export default function MenuItem({ title, imageUrl, size }) {
 
     )
 }
+
+export default withRouter(MenuItem)
